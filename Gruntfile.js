f4a9f24267912977e6d20d1dc0b78421b1734986
austinemackay <austin.e.mackay@gmail.com>
2016-03-12 15:08:15 -0500

    Added npm files and dependancies

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			src: {
+				files: ['src/', 'node_modules/bootstrap-sass'],
+				tasks: ['dev'],
+			}
+		},
+		sass: {
+			options: {
+				sourceMap: true,
+				includePaths: ['node_modules/bootstrap-sass/assets/stylesheets/']
+			},
+			dist: {
+				files: {
+					'static/css/main.css': 'src/scss/main.scss'
+				}
+			}
+		}
+	})
+
+	grunt.registerTask('default', ['dev', 'watch'])
+	grunt.registerTask('dev', ['sass'])
+}
--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, g
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+	return render_template("base.html")
 
 if __name__ == "__main__":
 	app.run(debug=True, port=8080)
