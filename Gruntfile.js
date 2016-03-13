f9552f7db2a06e529176cdae0ff589eefbb5dac6
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-03-12 19:13:55 -0500

    Upgraded gruntfile and changed base.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,6 @@ module.exports = function(grunt) {
 		},
 		sass: {
 			options: {
-				sourceMap: true,
 				includePaths: ['node_modules/bootstrap-sass/assets/stylesheets/']
 			},
 			dist: {
@@ -19,9 +18,22 @@ module.exports = function(grunt) {
 					'app/static/css/main.css': 'src/scss/main.scss'
 				}
 			}
-		}
+		},
+		postcss: {
+    		options: {
+    			map: true,
+	    		processors: [
+	        		require("pixrem")(), // add fallbacks for rem units
+	        		require("autoprefixer")({browsers: "last 2 versions"}), // add vendor prefixes
+	        		require("cssnano")() // minify the result
+	      		]
+    		},
+    		dist: {
+		    	src: "app/static/css/*.css"
+		    }
+    	}
 	})
 
 	grunt.registerTask('default', ['dev', 'watch'])
-	grunt.registerTask('dev', ['sass'])
+	grunt.registerTask('dev', ['sass', 'postcss'])
 }
