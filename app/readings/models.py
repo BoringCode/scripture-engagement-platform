ee786addaf816c82d2383ca4a97c16755ea9cc36
lindsayrobinso <lindsay_robinson@taylor.edu>
2016-03-16 13:52:24 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/readings/controllers.py
    #	app/readings/forms.py
    #	app/readings/models.py

--- app/readings/models.py
+++ app/readings/models.py
@@ -1,4 +1,48 @@
 from flask import g
 
 def find_reading(id):
-	return g.db.execute('SELECT * FROM reading WHERE id = :id', {"id": id}).fetchone() 
+	return g.db.execute('SELECT * FROM reading WHERE id = :id', {"id": id}).fetchone()
+
+def add_reading_to_db(id, name, creation_time,text, BG_passage_reference):
+    query = '''
+        INSERT INTO reading (id, name, creation_time, text, BG_passage_reference)
+        VALUES (:id, :name, :creation_time, :text, :BG_passage_reference)
+            '''
+    cursor = g.db.execute(query, {"id":id,"name":name,"creation_time":creation_time, "text":text, "BG_passage_reference":BG_passage_reference})
+    g.db.commit()
+    return cursor.rowcount
+
+def all_readings():
+    cursor = g.db.execute('select * from reading')
+    return cursor.fetchall()
+
+def new_reading(name, BG_passage_reference):
+    return g.db.execute('SELECT * FROM reading WHERE name = :name AND BG_passage_reference = :BG_passage_reference', {"name": name, "BG_passage_reference": BG_passage_reference}).fetchone()
+
+#delete readings
+def delete_reading(id):
+    delete_reading_content(id, False)
+    delete_reading_post(id, False)
+    delete_plan_reading(False, id)
+
+#delete_reading_post(id,False)
+    return g.db.execute('DELETE FROM reading WHERE id = :id', {"id": id}).fetchone()
+
+def delete_reading_content(reading_id, content_id):
+    return g.db.execute('DELETE FROM reading_content WHERE reading_id = :reading_id OR content_id = :content_id', {"reading_id": reading_id, "content_id": content_id}).fetchall()
+
+
+def delete_reading_post(reading_id,post_id):
+#return g.db.execute('DELETE FROM reading_post WHERE reading_id = :reading_id OR post_id = :post_id', {"reading_id": reading_id, "post_id": post_id}).fetchall()
+    return True
+
+def delete_plan_reading(plan_id, reading_id):
+    #return g.db.execute('DELETE FROM plan_reading WHERE reading_id = :reading_id OR plan_id = :plan_id', {"reading_id": reading_id, "plan_id": plan_id}).fetchall()
+    return True
+
+#update reading
+def update_reading(name, text, reference):
+    query ='''
+      UPDATE reading SET name=?, text=?, BG_passage_reference=? WHERE id = :id
+    '''
+    return g.db.execute(query, (name, text, reference))
