877d343da98c2e0449c8f5f09f52ec589e04a9e6
BoringCode <rosenfeld.bradley@gmail.com>
2016-04-27 16:02:02 -0400

    Logged out decorator

--- app/users/controllers.py
+++ app/users/controllers.py
@@ -1,4 +1,5 @@
 import flask
+from functools import wraps
 from flask import Blueprint, render_template, flash, redirect, url_for, g
 from werkzeug.exceptions import abort
 import flask.ext.login as flask_login
@@ -13,52 +14,43 @@ import app.users.forms as forms
 
 users = Blueprint('users', __name__)
 
+def logged_out_only(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		if g.user is not None and g.user.is_authenticated:
+			return redirect(url_for("index"))
+		return f(*args, **kwargs)
+	return decorated_function
+
 @users.route("/login/", methods=["GET", "POST"])
+@logged_out_only
 def login():
 	form = forms.Login()
-
-	if g.user is not None and g.user.is_authenticated:
-		return redirect(url_for('index'))
 
 	if form.validate_on_submit():
 		user = User(form.email.data, form.password.data)
 		if user.is_authenticated:
 			flask_login.login_user(user)
 
-			flash('Logged in successfully.')
+			flash("Welcome back " + user.first_name, "success")
 
-			next = flask.request.args.get('next')
-		    # next_is_valid should check if the user has valid
-		    # permission to access the `next` url
-			#if not next_is_valid(next):
-			#	return flask.abort(400)
-
-			return flask.redirect(next or flask.url_for('index'))
+			return flask.redirect(flask.url_for('index'))
 		flash("Cannot login with given information", "danger")
 	return render_template("users/login.html", form=form)
 
 @users.route("/register/", methods=["GET", "POST"])
+@logged_out_only
 def register():
 	form = forms.Register()
-
-	if g.user is not None and g.user.is_authenticated:
-		return redirect(url_for('index'))
 
 	if form.validate_on_submit():
 		user = register_user(form)
 		if user is not False and user.is_authenticated:
 			flask_login.login_user(user)
 
-			flash("Created user!")
+			flash("Welcome to the site " + user.first_name, "success")
 
-			next = flask.request.args.get('next')
-
-		    # next_is_valid should check if the user has valid
-		    # permission to access the `next` url
-			#if not next_is_valid(next):
-			#	return flask.abort(400)
-
-			return flask.redirect(next or flask.url_for('index'))
+			return flask.redirect(flask.url_for('index'))
 		flash("Can't create user", "danger")
 
 	return render_template("users/register.html", form=form)
@@ -66,9 +58,9 @@ def register():
 @users.route("/logout/")
 @flask_login.login_required
 def logout():
+	flash("Logged out", "success")
 	flask_login.logout_user()
 	return flask.redirect(flask.url_for("index"))
-
 
 
 @users.route("/profile/<user_id>/")
@@ -79,18 +71,6 @@ def profile(user_id):
 		return render_template("users/profile.html", user=user)
 	except:
 		return flask.abort(404)
-
-"""
-@login_manager.request_loader
-def request_loader(request):
-	user_id = request.form.get("email")
-	password = request.form.get("password")
-	if user_id is not None and password is not None and User.exists(user_id):
-		return User(user_id, password)
-	else:
-		return None
-"""
-
 
 @login_manager.user_loader
 def user_loader(user_id):
