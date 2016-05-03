c89b90681c7b17b8f262257f90a0dcf9f9513f46
farmerjon93 <farmerjon93@gmail.com>
2016-05-03 13:51:50 -0400

    updated templates/groups and groups/forms

--- app/groups/forms.py
+++ app/groups/forms.py
@@ -2,6 +2,11 @@ from flask.ext.wtf import Form
 from wtforms import StringField, TextField, TextAreaField, SubmitField, SelectField, DateField
 from app.groups.models import all_users, all_users_in_group
 
+class AddGroup(Form):
+    group_name = TextField
+    description = TextField
+    submit = SubmitField('Add Group')
+
 class AddUserToGroup(Form):
     user_select = SelectField('Select User')
     submit = SubmitField('Add User to Group')
@@ -14,3 +19,28 @@ class AddUserToGroup(Form):
             if user.id not in allGroupUsers.id:
                 users.append([str(user["id"]),user.username["username"],user.last_name["lastname"],user.first_name["firstname"]])
         self.user_select.choices = users
+
+class AddPlanToGroup(Form):
+    user_select = SelectField('Select Plan')
+    submit = SubmitField('Add Plan to Group')
+
+    def set_choices(self, group_id):
+        allPlans=all_plans()
+        allPlansGroups = all_plans_in_group(group_id)
+        plans = [[0,'select...']]
+        for plan in allPlans:
+            if plan.id not in allPlansGroups.id:
+                plans.append([str(plan["id"]),plan.name["name"],plan.description["description"]])
+        self.plan_select.choices = plans
+
+class ShowAllGroups(Form):
+    #List Groups on Static Page??
+    group_name = TextField
+    description = TextField
+    submit = SubmitField('Add Group')
+
+class ShowGroup(Form):
+    #List Group Details from DB.
+    group_name = TextField
+    description = TextField
+    submit = SubmitField('Add Group')
